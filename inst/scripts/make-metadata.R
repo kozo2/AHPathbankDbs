6577ebe0d3c80f93459678d156874ded24fc08f2
Kozo Nishida <kozo.nishida@gmail.com>
2020-12-13 10:04:33 +0900

    Create metadata.csv

--- inst/scripts/make-metadata.R
+++ inst/scripts/make-metadata.R
@@ -1,25 +1,38 @@
-species = c(""Homo sapiens", "Escherichia coli", "Mus musculus",
-    "Arabidopsis thaliana", "Saccharomyces cerevisiae", "Bos taurus", "Caenorhabditis elegans",
-    "Rattus norvegicus", "Drosophila melanogaster", "Pseudomonas aeruginosa")
+pathbank_species <- c("Homo sapiens", "Escherichia coli", "Mus musculus",
+    "Arabidopsis thaliana", "Saccharomyces cerevisiae", "Bos taurus",
+    "Caenorhabditis elegans", "Rattus norvegicus",
+    "Drosophila melanogaster", "Pseudomonas aeruginosa")
+
+pathbank_taxonomyids <-c("9606", "562", "10090",
+    "3702", "4932", "9913",
+    "6239", "10116",
+    "7227", "287")
+
+metabolite_titles <- paste("pathbank", gsub(" ", "_", pathbank_species), "metabolites.rda", sep = "_")
+protein_titles <- paste("pathbank", gsub(" ", "_", pathbank_species), "proteins.rda", sep = "_")
+titles <- c(metabolite_titles, protein_titles)
+
+metabolite_descriptions <- paste('Metabolite names linked to PathBank', pathbank_species, 'pathways (includes KEGG and ChEBI IDs)')
+protein_descriptions <- paste('Protein names linked to PathBank', pathbank_species, 'pathways (includes UniProt IDs)')
+descriptions <- c(metabolite_descriptions, protein_descriptions)
 
 meta <- data.frame(
-    Title = "Pathbank Db for ",
-    Description = paste0("RNA-seq data from study XYZ containing 10 normal ",
-			 "and 10 tumor samples represented as a",
-			 "SummarizedExperiment"),
-    BiocVersion = "3.4",
-    Genome = "GRCh38",
+    Title = titles,
+    Description = descriptions,
+    BiocVersion = "3.13",
+    Genome = NA,
     SourceType = "CSV",
-    SourceUrl = "http://www.path/to/original/data/file",
-    SourceVersion = "Jan 01 2016",
-    Species = "Homo sapiens",
-    TaxonomyId = 9606,
-    Coordinate_1_based = TRUE,
-    DataProvider = "GEO",
-    Maintainer = "Your Name <youremail@provider.com>",
-    RDataClass = "SummarizedExperiment",
+    SourceUrl = "https://pathbank.org/downloads",
+    SourceVersion = "Dec 13 2020",
+    Species = c(pathbank_species, pathbank_species),
+    TaxonomyId = c(pathbank_taxonomyids, pathbank_taxonomyids),
+    Coordinate_1_based = NA,
+    DataProvider = "PathBank",
+    Maintainer = "Kozo Nishida <kozo.nishida@gmail.com>",
+    RDataClass = "Tibble",
     DispatchClass = "Rda",
-    ResourceName = "FileName.rda"
+    RDataPath = paste0('AHPathbankDbs/', titles),
+    Tags = "Pathways:metabolites:proteins:HMDB:KEGG:ChEBI:Drugbank:CAS:Uniprot"
 )
 
 write.csv(meta, file="metadata.csv", row.names=FALSE)
